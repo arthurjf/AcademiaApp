9d71ab3475da9030ec68c3ae539c9a4e983e8fcc
arthurjf <arthur.310100@hotmail.com>
2023-04-01 11:40:37 -0300

    Layout da tela de login

---
+++ app/src/main/java/br/com/arthurjf/cursoandroid/academiaapp/view/MainActivity.kt
@@ -0,0 +1,28 @@
+package br.com.arthurjf.cursoandroid.academiaapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import br.com.arthurjf.cursoandroid.academiaapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonLogin = findViewById<Button>(R.id.buttonLogin)
+        val editTextUsername = findViewById<EditText>(R.id.editTextUsername)
+        val editTextPassword = findViewById<EditText>(R.id.editTextPassword)
+
+
+        buttonLogin.setOnClickListener{
+            if(editTextUsername.text.toString().equals("admin") && editTextPassword.text.toString().equals("admin")){
+                Toast.makeText(this, "Logado com sucesso!", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "Usuário ou senha incorreta!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
